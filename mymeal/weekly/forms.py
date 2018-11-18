f4f72dba63a45faa7fb1544a436e00b913dbea0a
Steven Devonport <steven.devonport@live.co.uk>
2018-11-18 11:49:26 +0000

    modified weekly planner page

---
+++ mymeal/weekly/forms.py
@@ -0,0 +1,31 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, SelectField
+from wtforms.validators import DataRequired
+from mymeal.models import Recipe
+
+
+class NewWeekForm(FlaskForm):
+    recipe = StringField('Meal', validators=[DataRequired()], render_kw={'placeholder': 'Name of meal'})
+    submit = SubmitField('Add meal')
+
+
+class ThisWeekForm(FlaskForm):
+    aSelect = StringField('Meal', validators=[DataRequired()], render_kw={'placeholder': 'Name of meal'})
+    mondaySelect = SelectField('Monday')
+    tuesdaySelect = SelectField('Tuesday')
+    wednesdaySelect = SelectField('Wednesday')
+    thursdaySelect = SelectField('Thursday')
+    fridaySelect = SelectField('Friday')
+    saturdaySelect = SelectField('Saturday')
+    sundaySelect = SelectField('Sunday')
+    submit = SubmitField('Create Ingredients List')
+
+    def __init__(self, *args, **kwargs):
+        super(ThisWeekForm, self).__init__(*args, **kwargs)
+        self.mondaySelect.choices = [(recipe.id,recipe.title) for recipe in Recipe.query.all()]
+        self.tuesdaySelect.choices = [(recipe.id, recipe.title) for recipe in Recipe.query.all()]
+        self.wednesdaySelect.choices = [(recipe.id, recipe.title) for recipe in Recipe.query.all()]
+        self.thursdaySelect.choices = [(recipe.id, recipe.title) for recipe in Recipe.query.all()]
+        self.fridaySelect.choices = [(recipe.id, recipe.title) for recipe in Recipe.query.all()]
+        self.saturdaySelect.choices = [(recipe.id, recipe.title) for recipe in Recipe.query.all()]
+        self.sundaySelect.choices = [(recipe.id, recipe.title) for recipe in Recipe.query.all()]
