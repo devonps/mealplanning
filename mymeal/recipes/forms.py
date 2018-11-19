12ea704b7654cc6a8f334d45a121002b8e691cbd
Steven Devonport <steven.devonport@live.co.uk>
2018-11-19 09:43:46 +0000

    updated display for weekly planner

--- mymeal/recipes/forms.py
+++ mymeal/recipes/forms.py
@@ -8,7 +8,7 @@ class RecipeForm(FlaskForm):
     serves_choices = [('1','1'), ('2', '2'),('3', '3'),('4', '4'),('5', '5'),('6', '6'),('7', '7'),('8', '8'),('9', '9')]
     serves = SelectField('Serves How Many', choices=serves_choices, validators=[DataRequired()])
     description = TextAreaField('Description', validators=[DataRequired()])
-    recipe_type_choices = [('snack', 'Snack'), ('lunch', 'Lunch'),('dinner', 'Dinner'),('desert', 'Desert')]
+    recipe_type_choices = [('snack', 'Snack'), ('lunch', 'Lunch'),('dinner', 'Dinner'),('dessert', 'Dessert')]
     recipe_type = SelectField('Recipe Type', choices=recipe_type_choices, validators=[DataRequired()])
     recipe_book = StringField('Recipe Book')
     recipe_book_page = IntegerField('Page Number')
