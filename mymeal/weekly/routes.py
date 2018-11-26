7c8be9cd1289b90a1854cae66c1db23b81c5adc8
Steven Devonport <steven.devonport@live.co.uk>
2018-11-26 14:16:29 +0000

    Weekly meal choices stored in meal plan table

--- mymeal/weekly/routes.py
+++ mymeal/weekly/routes.py
@@ -1,9 +1,8 @@
-from flask import render_template, Blueprint, request, redirect, url_for, flash
-from flask_login import login_required
-from mymeal.weekly.forms import ThisWeekForm, ThisWeeksIngredients
-from mymeal.models import Ingredient
-
-from sqlalchemy import and_, asc
+from flask import render_template, Blueprint, request
+from flask_login import login_required, current_user
+from mymeal.weekly.forms import ThisWeekForm
+from mymeal.models import Mealplan
+from mymeal import db
 
 weekly = Blueprint('weekly', __name__)
 
@@ -11,39 +10,58 @@ weekly = Blueprint('weekly', __name__)
 @weekly.route('/week/new', methods=['GET', 'POST'])
 @login_required
 def new_week():
-    ingredients = ''
     form = ThisWeekForm()
     if request.method == 'POST':
-        return redirect(url_for('weekly.week_ingredients'))
-    return render_template('thisweek.html', title='This Week', form=form, ingredients=ingredients)
+        # write out week choices to Mealplan table
+        week = current_user.meal_plan_count + 1
+        week_description = 'week ' + str(week)
+        dirty_table = False
+
+        if int(form.saturdaySelect.data) != 0:
+            dirty_table=True
+            saturdaymeal = Mealplan(week=week, day_of_meal='saturday', meal_id=form.saturdaySelect.data,
+                                    week_description=week_description, creator=current_user)
+            db.session.add(saturdaymeal)
+
+        if int(form.sundaySelect.data) != 0:
+            dirty_table = True
+            sundaymeal = Mealplan(week=week, day_of_meal='sunday', meal_id=form.sundaySelect.data, week_description=week_description, creator=current_user)
+            db.session.add(sundaymeal)
+
+        if int(form.mondaySelect.data) != 0:
+            dirty_table = True
+            mondaymeal = Mealplan(week=week, day_of_meal='monday', meal_id=form.mondaySelect.data, week_description=week_description, creator=current_user)
+            db.session.add(mondaymeal)
+
+        if int(form.tuesdaySelect.data) != 0:
+            dirty_table = True
+            tuesdaymeal = Mealplan(week=week, day_of_meal='tuesday', meal_id=form.tuesdaySelect.data, week_description=week_description, creator=current_user)
+            db.session.add(tuesdaymeal)
+
+        if int(form.wednesdaySelect.data) != 0:
+            dirty_table = True
+            wednesdaymeal = Mealplan(week=week, day_of_meal='wednesday', meal_id=form.wednesdaySelect.data, week_description=week_description, creator=current_user)
+            db.session.add(wednesdaymeal)
+
+        if int(form.thursdaySelect.data) != 0:
+            dirty_table = True
+            thursdaymeal = Mealplan(week=week, day_of_meal='thursday', meal_id=form.thursdaySelect.data, week_description=week_description, creator=current_user)
+            db.session.add(thursdaymeal)
+
+        if int(form.fridaySelect.data) != 0:
+            dirty_table = True
+            fridaymeal = Mealplan(week=week, day_of_meal='friday', meal_id=form.fridaySelect.data, week_description=week_description, creator=current_user)
+            db.session.add(fridaymeal)
+
+        if dirty_table:
+            setattr(current_user, 'meal_plan_count', week)
+            db.session.commit()
+
+    return render_template('thisweek.html', title='This Week', form=form)
 
 
 @weekly.route('/week/ingredients', methods=['GET', 'POST'])
 @login_required
 def week_ingredients():
-    form = ThisWeeksIngredients()
-    r1 = int(request.form['saturdaySelect'])
-    r2 = int(request.form['sundaySelect'])
-    r3 = int(request.form['mondaySelect'])
-    r4 = int(request.form['tuesdaySelect'])
-    r5 = int(request.form['wednesdaySelect'])
-    r6 = int(request.form['thursdaySelect'])
-    r7 = int(request.form['fridaySelect'])
-
-    if request.method == 'POST':
-
-            supermarket = get_all_ingredients_for_shopping_location('supermarket', r1, r2, r3, r4, r5, r6, r7)
-            butchers = get_all_ingredients_for_shopping_location('butcher', r1, r2, r3, r4, r5, r6, r7)
-            greengrocers = get_all_ingredients_for_shopping_location('greengrocer', r1, r2, r3, r4, r5, r6, r7)
-
-            return render_template('weekly_ingredients.html', title='Ingredients', form=form, supermarket=supermarket,
+    return render_template('weekly_ingredients.html', title='Ingredients', form=form, supermarket=supermarket,
                                    butchers=butchers, greengrocers=greengrocers)
-    elif request.method == 'GET':
-        flash('Ingredients email has been sent', 'success')
-        return redirect(url_for('main.home'))
-
-
-def get_all_ingredients_for_shopping_location(location, r1, r2, r3, r4, r5, r6, r7):
-    return Ingredient.query.filter(
-            and_(Ingredient.purchased_at == location, Ingredient.recipe_id.in_([r1, r2, r3, r4, r5, r6, r7]))).order_by(
-            asc(Ingredient.name))
